4399b5f55d1419c1c768999e82100ab02429f7cd
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-21 14:50:58 +0530

    275

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-    return <h1>The Welcome page</h1>;
+  return (
+    <section>
+      <h1>The Welcome page</h1>
+      <Route path="/products">
+        <p>Welcome, new USer</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
